7cc6d03e2065cfb8aef5ad48abd467d92c93e174
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2024-07-03 19:40:56 +0200

    fix typo

--- Hourglass/AppEntry.cs
+++ Hourglass/AppEntry.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media.Animation;
-using Syste.IO;
+using System.IO;
 
 using Extensions;
 using Managers;
