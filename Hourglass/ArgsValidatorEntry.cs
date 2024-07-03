b5ac4e3175223032c93608ee03dac1d82cf87118
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2024-07-04 00:31:25 +0200

    build hourglass_args_validator

---
+++ Hourglass/ArgsValidatorEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+using Hourglass.Timing;
+
+namespace Hourglass
+{
+    internal class ArgsValidatorEntry
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            CommandLineArguments arguments = CommandLineArguments.Parse(args);
+            string toWrite = "{\"result\":" + (arguments.HasParseError ? "false" : "true") + ",";
+            toWrite = toWrite + "\"timeStrings\":[";
+
+            if (!arguments.HasParseError)
+            {
+                int index = arguments.TimerStart.OfType<TimerStart>().Count();
+                foreach (var timerStart in arguments.TimerStart.OfType<TimerStart>())
+                {
+                    toWrite = toWrite + "\"" + timerStart.ToString() + "\"" + (index > 1 ? "," : "");
+                    index = index - 1;
+                }
+            }
+            toWrite = toWrite + "]}";
+
+            Console.WriteLine(toWrite);
+            return;
+        }
+    }
+}
